b2b6a5fe237bdbd05511a17646078fd6462d593c
pushparaja <appumoorthy15@gmail.com>
2021-08-15 20:55:38 +0530

    Delete Project-0 directory

--- Project-0/src/main/java/com/revature/Exception/AccountBalanceException.java
+++
@@ -1,8 +0,0 @@
-package com.revature.Exception;
-
-public class AccountBalanceException extends Exception {
-	public AccountBalanceException(double balance) {
-		super(String.valueOf(balance));
-	}
-
-}
--- Project-0/src/main/java/com/revature/entity/Type.java
+++
@@ -1,6 +0,0 @@
-package com.revature.entity;
-
-public enum Type {
-	CREDIT, DEBIT
-
-}
